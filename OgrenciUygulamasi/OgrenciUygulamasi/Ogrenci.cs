35afd0c7bc95fe6b66bbd0338854db1200f2bff0
fatihbereketinan <fatihbereketinan@gmail.com>
2022-08-01 21:55:49 +0300

    CSharp_Classlar_OgrenciUygulamasi

---
+++ OgrenciUygulamasi/OgrenciUygulamasi/Ogrenci.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OgrenciUygulamasi
+{
+    public class Ogrenci
+    {
+        private int ogrenciNo;
+        private string isim;
+        private string soyisim;
+        private double vizenot1;
+        private double vizenot2;
+        private double finalnot;
+        private string okulismi;
+
+        public Ogrenci(int _ogrenciNo, string _isim, string _soyisim, double _vizenot1, double _vizenot2, double _finalnot, string _okulismi)
+        {
+            ogrenciNo = _ogrenciNo;
+            isim = _isim;
+            soyisim = _soyisim;
+            vizenot1 = _vizenot1;
+            vizenot2 = _vizenot2;
+            finalnot = _finalnot;
+            okulismi = _okulismi;
+        }
+
+        public void ogrenciBilgileriGoster()
+        {
+            Console.WriteLine("Öğrencinin Okul Numarası: " + ogrenciNo);
+            Console.WriteLine("Öğrencinin Adı ve Soyadı: " + isim + " " + soyisim);
+            Console.WriteLine("Öğrencinin Vize1, Vize2 ve Final Notları: " + vizenot1 + "," + vizenot2 + "," + finalnot);
+            Console.WriteLine("Öğrencinin Okul Ad: "+okulismi);
+            Console.WriteLine("");
+        }
+        public void ogrenciOrtalamasiBul()
+        {
+            Console.WriteLine("Öğrencinin not ortalaması: "+((0.4*((vizenot1+vizenot2)/2))+(0.6*finalnot)));
+            Console.WriteLine("");
+        }
+        public void okulGetir()
+        {
+            Console.WriteLine("Öğrencinin eğitim gördüğü okul: " + okulismi);
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ OgrenciUygulamasi/OgrenciUygulamasi/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace OgrenciUygulamasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool kontrol = true;
+            Ogrenci ogrenci1 = new Ogrenci(100, "Fatih Bereket", "İnan", 50, 80, 100, "Erciyes Üniversitesi");
+            while (kontrol)
+            {
+                int secim= ogrenciİslemi();
+                switch (secim)
+                {
+                    case 1:
+                        ogrenci1.ogrenciBilgileriGoster();
+                        break;
+                    case 2:
+                        ogrenci1.ogrenciOrtalamasiBul();
+                        break;
+                    case 3:
+                        ogrenci1.okulGetir();
+                        break;
+                    case 4:
+                        Console.WriteLine("Sistemden çıkış yapıldı.");
+                        kontrol = false;
+                        break;
+                    default:
+                        Console.WriteLine("Hatalı seçim yaptınız...");
+                        Console.WriteLine("***************************");
+                        break;
+                }
+            }
+        }
+        static int ogrenciİslemi()
+        {
+            Console.WriteLine("1- Öğrenci Bilgilerini Göster.");
+            Console.WriteLine("2- Öğrenci Not Ortalamasını Göster.");
+            Console.WriteLine("3- Öğrencinin Eğitim Gördüğü Okulu Göster.");
+            Console.WriteLine("4- Çıkış Yap.");
+            Console.Write("Lütfen bir işlemi seçiniz: ");
+            int secim = Convert.ToInt32(Console.ReadLine());
+            return secim;
+        }
+    }
+}
